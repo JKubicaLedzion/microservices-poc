241338c543311049dc334a86b4e6e297ff5e9f5c
Kubica Ledzion <justyna.kubica---ledzion@dxc.com>
2019-06-27 11:09:53 +0200

    Hystrix fallback methods added to controller.

--- customer-service/src/main/java/com/ledzion/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/ledzion/customerservice/controller/CustomerController.java
@@ -2,11 +2,13 @@ package com.ledzion.customerservice.controller;
 
 import com.ledzion.customerservice.model.Customer;
 import com.ledzion.customerservice.service.CustomerService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,9 @@ public class CustomerController {
 
     private static final String CUSTOMER_NOT_FOUND = "Customer not found.";
 
+    private static final String SERVICE_UNAVAILABLE_ERROR_MESSAGE =
+            "No Response From Customer Service at this moment. " + " Service will be back shortly.";
+
     private CustomerService customerService;
 
     @Autowired
@@ -32,7 +37,8 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @RequestMapping(method = GET, value = "/{id}", produces = APPLICATION_JSON_UTF8_VALUE)
+    @HystrixCommand(fallbackMethod = "getCustomerByIdFallback")
+    @GetMapping(value = "/{id}", produces = APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity getCustomerById(@PathVariable("id") long id) {
         LOGGER.debug("Getting customer with id {}.", id);
         Optional<Customer> customer = customerService.getCustomerById(id);
@@ -41,12 +47,23 @@ public class CustomerController {
                 : ResponseEntity.status(HttpStatus.NOT_FOUND).body(CUSTOMER_NOT_FOUND);
     }
 
-    @RequestMapping(method = GET, produces = APPLICATION_JSON_UTF8_VALUE)
+    @HystrixCommand(fallbackMethod = "getAllCustomersFallback")
+    @GetMapping(produces = APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity getAllCustomers() {
         LOGGER.debug("Getting all customers.");
         List<Customer> customers = customerService.getAllCustomers();
         return customers.isEmpty()
                 ? ResponseEntity.status(HttpStatus.NOT_FOUND).body(CUSTOMER_NOT_FOUND)
                 : ResponseEntity.status(HttpStatus.OK).body(customers);
+    }
+
+    @SuppressWarnings("unused")
+    public ResponseEntity getCustomerByIdFallback(@PathVariable("id") long id) {
+        return ResponseEntity.ok().body( SERVICE_UNAVAILABLE_ERROR_MESSAGE );
+    }
+
+    @SuppressWarnings("unused")
+    public ResponseEntity getAllCustomersFallback(@PathVariable("id") long id) {
+        return ResponseEntity.ok().body( SERVICE_UNAVAILABLE_ERROR_MESSAGE );
     }
 }
