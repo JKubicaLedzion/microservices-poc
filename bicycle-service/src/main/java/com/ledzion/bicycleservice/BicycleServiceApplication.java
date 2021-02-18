a373d9324e36bce3b850ae0d861061164f03e3d5
JKubicaLedzion <j.kubica.ledzion@gmail.com>
2021-02-18 10:26:15 +0100

    Swagger2 implemented in booking-service, customer-service, bicycle-service.

--- bicycle-service/src/main/java/com/ledzion/bicycleservice/BicycleServiceApplication.java
+++ bicycle-service/src/main/java/com/ledzion/bicycleservice/BicycleServiceApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 @EnableHystrixDashboard
 @EnableCircuitBreaker
